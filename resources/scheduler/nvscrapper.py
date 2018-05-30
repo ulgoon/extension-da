c79798fd5c5e7bfb07c7a12cdb2ea19a0b93a041
ulgoon <ulgoon89@gmail.com>
2018-05-30 22:02:24 +0900

    add feature: insert to mlab(mongodb)

--- resources/scheduler/nvscrapper.py
+++ resources/scheduler/nvscrapper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from bs4 import BeautifulSoup as btfs
+from pymongo import MongoClient
 
 
 def scrape():
@@ -18,7 +19,25 @@ def scrape():
         rank = li.find('span', attrs={"class":"ah_r"}).text
         keyword = li.find('span', attrs={"class":"ah_k"}).text
         results[rank] =  keyword
-    return print(results)
+    return results
 
 if __name__ == "__main__":
-    scrape()
+    results_dict = scrape()
+    try:
+        mongo_url = "mongodb://strongadmin:12341234@ds123500.mlab.com:23500/collections"
+        client = MongoClient(mongo_url)
+        db = client.collections
+        nv_query_collection = db.realtimekeyword
+        nv_query_collection.insert(results_dict)
+        print("success")
+    except:
+        print("failed. try again")
+
+
+
+
+
+
+
+
+
