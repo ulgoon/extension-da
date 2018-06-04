860ed4f6bf0b4600f85f248665caaed11d1187d3
ulgoon <ulgoon89@gmail.com>
2018-06-04 17:48:44 +0900

    edit server.py: json serve complete

--- resources/flaskapiserver/server.py
+++ resources/flaskapiserver/server.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import json, Response
+from pymongo import MongoClient
+from bson import json_util
 # requirement: flask($ pip install flask)
 
 # define app using Flask
@@ -10,9 +13,24 @@ def index():
     return "hello"
 
 # route for api server
+# load data from mlab with pymongo
+# bson to json => serve over /api/v1/naver
 @app.route('/api/v1/naver')
 def get_keyword():
-    return "keyword will shows here"
+    # customize your uri
+    uri = "mongodb://strongadmin:12341234@ds123500.mlab.com:23500/collections"
+    # connect client
+    client = MongoClient(uri)
+    db = client.collections
+    query_list = db.nvquerylist
+    # make dictionary from loaded data from query_list
+    results = {"data":
+            [
+                item for item in query_list.find({})
+            ]
+        }
+    # serve json data over http
+    return Response(json_util.dumps(results), mimetype="application/json")
 
 # flask run with shell
 if __name__ == '__main__':
